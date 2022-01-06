829b7725166353a818051ad09b27106c2a6d0fe9
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 12:51:03 +0900

    로그인 버튼이 눌리면, 입력한 이메일과 비밀번호 가져오기

--- app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
@@ -2,10 +2,18 @@ package com.neppplus.self_apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+
+            val inputEmail = edtEmail.text.toString()
+            val inputPw = edtPassword.text.toString()
+
+        }
     }
 }
