18878abbd43f177886c42a8d4e9ada9cf318b1cb
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 18:14:41 +0900

    화면에 안보임. 서버에 다녀오는건 리사이클러뷰 세팅보다 오래걸리기 때문..

--- app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
@@ -3,14 +3,19 @@ package com.neppplus.self_apipractice_20220106
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telecom.Call
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.neppplus.self_apipractice_20220106.adapters.ReviewAdapter
 import com.neppplus.self_apipractice_20220106.models.BasicResponse
 import com.neppplus.self_apipractice_20220106.models.ReviewData
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Response
 import javax.security.auth.callback.Callback
 
 class MainActivity : BaseActivity() {
 
     val mReviewList = ArrayList<ReviewData>()
+
+    lateinit var mReviewAdapter : ReviewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,5 +41,10 @@ class MainActivity : BaseActivity() {
 
 
         })
+
+        mReviewAdapter = ReviewAdapter(mContext, mReviewList)
+        reviewRecyclerView.adapter = mReviewAdapter
+        reviewRecyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+
     }
 }
