24fbb1b039ea46c56e523fea4fea4d8d7ca68b28
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 18:08:06 +0900

    모든 리뷰 목록 받아와야하는데, 그 기능이 아직 없음

--- app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
@@ -2,10 +2,17 @@ package com.neppplus.self_apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.self_apipractice_20220106.models.ReviewData
 
 class MainActivity : BaseActivity() {
+
+    val mReviewList = ArrayList<ReviewData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        apiList.getre
+
     }
 }
