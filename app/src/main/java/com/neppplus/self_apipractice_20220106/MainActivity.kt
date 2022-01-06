82fa40200e775f04ab932ac5bf1d7be40c641544
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 12:56:28 +0900

    request날리고 난 후의 응답에 따라 성공실패 토스트로 띄우기

--- app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
@@ -2,9 +2,14 @@ package com.neppplus.self_apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.neppplus.self_apipractice_20220106.api.APIList
 import com.neppplus.self_apipractice_20220106.api.ServerAPI
+import com.neppplus.self_apipractice_20220106.models.BasicResponse
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +24,26 @@ class MainActivity : AppCompatActivity() {
             val retrofit = ServerAPI.getRetrofit()
             val apiList = retrofit.create(APIList::class.java)
 
-            apiList.postRequestLogin(inputEmail, inputPw)
+            apiList.postRequestLogin(inputEmail, inputPw).enqueue(object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        Toast.makeText(this@MainActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+                        Toast.makeText(this@MainActivity, "로그인 실패", Toast.LENGTH_SHORT).show()
+                    }
+                    
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+                    
+                }
+
+
+            })
 
         }
     }
