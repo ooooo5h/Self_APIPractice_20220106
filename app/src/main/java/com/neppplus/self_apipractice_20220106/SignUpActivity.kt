821aa6df6d3527129b38f1da2c411c155a1b25cd
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 13:15:43 +0900

    회원가입 기능을 호출 후 응답처리까지 완료

--- app/src/main/java/com/neppplus/self_apipractice_20220106/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/SignUpActivity.kt
@@ -2,9 +2,15 @@ package com.neppplus.self_apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.neppplus.self_apipractice_20220106.api.APIList
 import com.neppplus.self_apipractice_20220106.api.ServerAPI
+import com.neppplus.self_apipractice_20220106.models.BasicResponse
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +27,31 @@ class SignUpActivity : AppCompatActivity() {
             val retrofit = ServerAPI.getRetrofit()
             val apiList = retrofit.create(APIList::class.java)
 
-            apiList.putRequestSignUp(inputEmail, inputPw, inputNickname, inputPhoneNum)
+            apiList.putRequestSignUp(inputEmail, inputPw, inputNickname, inputPhoneNum).enqueue(object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+                    if (response.isSuccessful) {
+
+                        val br = response.body()!!
+                        val signUpNickname = br.data.user.nick_name
+                        Toast.makeText(this@SignUpActivity, "${signUpNickname}님 회원가입이 성공적으로 되었습니다.", Toast.LENGTH_SHORT).show()
+
+                        finish()
+                    }
+                    else {
+
+                        val testStr = response.errorBody()!!.string()
+                        Log.d("문제 생김", testStr)
+                    }
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+            })
 
         }
     }
