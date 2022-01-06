ea1f1792440de4d162a3e7403db44a4dd302c97c
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 17:48:02 +0900

    메인액티비티 생성

---
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.self_apipractice_20220106
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
