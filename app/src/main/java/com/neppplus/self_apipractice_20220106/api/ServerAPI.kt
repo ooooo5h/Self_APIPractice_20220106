80383365dd7026fb9ddcb9f7b1ad3df96d3c914f
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 12:39:28 +0900

    Retrofit : ServerAPI.getRetrofit() 함수 생성   - 비어있다면 채워서 리턴

--- app/src/main/java/com/neppplus/self_apipractice_20220106/api/ServerAPI.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/api/ServerAPI.kt
@@ -1,6 +1,7 @@
 package com.neppplus.self_apipractice_20220106.api
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ServerAPI {
 
@@ -8,5 +9,19 @@ class ServerAPI {
 
         private val BASE_URL = "https://api.gudoc.in"
         private var retrofit : Retrofit? = null
+
+        fun getRetrofit() : Retrofit {
+
+            if (retrofit == null) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+
+            return retrofit!!
+
+
+        }
     }
 }
