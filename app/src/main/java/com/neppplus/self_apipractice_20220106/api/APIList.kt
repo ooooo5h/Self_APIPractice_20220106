ce1aff37e652cb87366855da2a95742779ab66bc
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 12:44:06 +0900

    받아올 응답이 없네? 응답 만들어야함

--- app/src/main/java/com/neppplus/self_apipractice_20220106/api/APIList.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/api/APIList.kt
@@ -1,5 +1,6 @@
 package com.neppplus.self_apipractice_20220106.api
 
+import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
@@ -12,5 +13,5 @@ interface APIList {
     fun postRequestLogin(
         @Field("email") email : String,
         @Field("password") pw : String,
-)
+) : Call
 }
