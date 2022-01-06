91b5f39fee345df4d78f3780fe9616451b830cb7
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 13:08:35 +0900

    Retrofit 기능 목록 중 회원가입이 없어서 호출을 못함

--- app/src/main/java/com/neppplus/self_apipractice_20220106/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/SignUpActivity.kt
@@ -2,6 +2,8 @@ package com.neppplus.self_apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.self_apipractice_20220106.api.APIList
+import com.neppplus.self_apipractice_20220106.api.ServerAPI
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -15,6 +17,11 @@ class SignUpActivity : AppCompatActivity() {
             val inputPw = edtPassword.text.toString()
             val inputNickname = edtNickname.text.toString()
             val inputPhoneNum = edtPhone.text.toString()
+
+            val retrofit = ServerAPI.getRetrofit()
+            val apiList = retrofit.create(APIList::class.java)
+
+            apiList.
 
         }
     }
