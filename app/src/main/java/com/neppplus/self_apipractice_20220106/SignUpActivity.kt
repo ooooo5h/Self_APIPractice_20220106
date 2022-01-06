c6c5b2c018a7c219c4ce15a35da63cfe5dccde64
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 13:07:32 +0900

    회원가입UI에서 회원가입 버튼이 눌리면 입력한 값 변수로 저장

--- app/src/main/java/com/neppplus/self_apipractice_20220106/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/SignUpActivity.kt
@@ -2,10 +2,20 @@ package com.neppplus.self_apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        btnSignUp.setOnClickListener {
+
+            val inputEmail = edtEmail.text.toString()
+            val inputPw = edtPassword.text.toString()
+            val inputNickname = edtNickname.text.toString()
+            val inputPhoneNum = edtPhone.text.toString()
+
+        }
     }
 }
