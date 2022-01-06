2c01312e83170c896337de603b604251c2b9711b
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 12:54:14 +0900

    로그인 버튼이 눌리면, 입력한 이메일과 비밀번호 가져오고, API연결도구 생성하고, retrofit.create(APIList::class.java)한 후 입력된 값으로 실제 로그인 기능에 request 날리기

--- app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
@@ -2,6 +2,8 @@ package com.neppplus.self_apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.self_apipractice_20220106.api.APIList
+import com.neppplus.self_apipractice_20220106.api.ServerAPI
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,11 @@ class MainActivity : AppCompatActivity() {
 
             val inputEmail = edtEmail.text.toString()
             val inputPw = edtPassword.text.toString()
+
+            val retrofit = ServerAPI.getRetrofit()
+            val apiList = retrofit.create(APIList::class.java)
+
+            apiList.postRequestLogin(inputEmail, inputPw)
 
         }
     }
