2272e1ab780e2c5ca7fecf9831d6290b86f3eaef
ooooo5h <ooooo5h91@gmail.com>
2022-01-06 12:43:19 +0900

    인터페이스 로그인 정의

---
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/api/APIList.kt
@@ -0,0 +1,16 @@
+package com.neppplus.self_apipractice_20220106.api
+
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface APIList {
+
+//    * 로그인
+    @FormUrlEncoded
+    @POST("/user")
+    fun postRequestLogin(
+        @Field("email") email : String,
+        @Field("password") pw : String,
+)
+}
