42549f3543158d5cea419a9c04ffe21962f5fca9
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 17:54:15 +0900

    첨부된 데이터를 이동된 화면에서 받았는데, 리뷰상세보기하는 api 기능이 없네?

--- app/src/main/java/com/neppplus/self_apipractice_20220106/ViewReviewActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/ViewReviewActivity.kt
@@ -2,10 +2,15 @@ package com.neppplus.self_apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.self_apipractice_20220106.models.ReviewData
 
 class ViewReviewActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_review)
+
+        val reviewData = intent.getSerializableExtra("review") as ReviewData
+
+        apiList.
     }
 }
