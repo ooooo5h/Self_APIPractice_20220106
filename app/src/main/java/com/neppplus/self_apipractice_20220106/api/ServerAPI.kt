832cb269e921f578cb81b07ab06659818160b3dd
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 12:53:18 +0900

    토큰은 여러가지 API 함수에서 필요로 한다 => 매번 ContextUtil.getToken()을 작성해야하나는 이야기 => 레트로핏 객체 생성 이전에(api 요청이 이뤄지기 전에) 헤더를 추가해주자

--- app/src/main/java/com/neppplus/self_apipractice_20220106/api/ServerAPI.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/api/ServerAPI.kt
@@ -1,5 +1,8 @@
 package com.neppplus.self_apipractice_20220106.api
 
+import android.content.Context
+import com.neppplus.self_apipractice_20220106.utils.ContextUtil
+import okhttp3.Interceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,9 +13,20 @@ class ServerAPI {
         private val BASE_URL = "https://api.gudoc.in"
         private var retrofit : Retrofit? = null
 
-        fun getRetrofit() : Retrofit {
+        fun getRetrofit(context: Context) : Retrofit {
 
             if (retrofit == null) {
+
+                val interceptor = Interceptor{
+                    with(it) {
+
+                        val newRequest = request().newBuilder()
+                            .addHeader("X-Http-Token", ContextUtil.getToken(context))
+                            .build()
+
+                        proceed(newRequest)
+                    }
+                }
                 retrofit = Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
