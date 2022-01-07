89f09d04608b8416b9a5009acc2dfb285dd13cfd
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 12:45:16 +0900

    내 정보 가져오기에 필요한 토큰값을 넣고, 서버로부터 응답 받아옴

--- app/src/main/java/com/neppplus/self_apipractice_20220106/MyProfileActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/MyProfileActivity.kt
@@ -2,12 +2,27 @@ package com.neppplus.self_apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.self_apipractice_20220106.models.BasicResponse
+import com.neppplus.self_apipractice_20220106.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyProfileActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
 
-        apiList.getRequestMyInfo(//토큰을 가져와야하는군)
+        apiList.getRequestMyInfo(ContextUtil.getToken(mContext)).enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+
+        })
     }
 }
