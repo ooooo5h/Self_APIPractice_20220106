0d852f5e5dbbacefe0696f0b20f6dde28146f96f
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 12:34:26 +0900

    메인화면에서 내프로필보기 버튼 누르면, 화면이동으로 내 프로필 화면 이동하는 UI구현

--- app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.self_apipractice_20220106
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telecom.Call
@@ -20,6 +21,13 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMyProfile.setOnClickListener {
+
+            val myIntent = Intent(mContext,MyProfileActivity::class.java)
+            startActivity(myIntent)
+        }
+
 
         apiList.getRequestAllReview().enqueue(object : retrofit2.Callback<BasicResponse>{
             override fun onResponse(
---
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/MyProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.self_apipractice_20220106
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyProfileActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_profile)
+    }
+}
