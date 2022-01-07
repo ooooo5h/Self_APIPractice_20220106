2f2a9ac2248230bda0029c615ef04816e7d2da53
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 18:09:57 +0900

    FCM 연결 후 기기 토큰값 발급_로그로

--- app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telecom.Call
+import android.util.Log
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.firebase.messaging.FirebaseMessaging
 import com.neppplus.self_apipractice_20220106.adapters.ReviewAdapter
 import com.neppplus.self_apipractice_20220106.models.BasicResponse
 import com.neppplus.self_apipractice_20220106.models.ReviewData
@@ -21,6 +23,15 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener {
+
+            if (it.isSuccessful) {
+
+                val deviceToken = it.result!!
+                Log.d("FCM토큰", deviceToken)
+            }
+        }
 
         btnViewProduct.setOnClickListener {
 
