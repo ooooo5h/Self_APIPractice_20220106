8c878ddd43bfca4f2908fefad524913eb8a62824
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 17:39:56 +0900

    이전 화면에서 넘겨준 데이터를 변수에 저장했고, 리뷰등록버튼이 눌리면 해야하는 일 중 필요 변수들 생성

--- app/src/main/java/com/neppplus/self_apipractice_20220106/EditReviewActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/EditReviewActivity.kt
@@ -2,10 +2,22 @@ package com.neppplus.self_apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.self_apipractice_20220106.models.ProductData
+import kotlinx.android.synthetic.main.activity_edit_review.*
 
 class EditReviewActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_review)
+
+        val product = intent.getSerializableExtra("product") as ProductData
+
+        btnSubmitReview.setOnClickListener {
+
+            val inputTitle = edtTitle.text.toString()
+            val inputContent = edtContent.text.toString()
+            val inputScore = ratingBar.rating
+
+        }
     }
 }
