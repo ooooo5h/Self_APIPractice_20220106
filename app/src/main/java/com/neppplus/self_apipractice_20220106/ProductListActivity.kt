3e5d449bb75bfa467c3ee40008d90870a1b4662d
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 13:32:04 +0900

    어댑터와 리싸이클러뷰 연결

--- app/src/main/java/com/neppplus/self_apipractice_20220106/ProductListActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/ProductListActivity.kt
@@ -2,8 +2,11 @@ package com.neppplus.self_apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.neppplus.self_apipractice_20220106.adapters.ProductAdapter
 import com.neppplus.self_apipractice_20220106.models.BasicResponse
 import com.neppplus.self_apipractice_20220106.models.ProductData
+import kotlinx.android.synthetic.main.activity_product_list.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -11,6 +14,8 @@ import retrofit2.Response
 class ProductListActivity : BaseActivity() {
 
     val mProductList = ArrayList<ProductData>()
+
+    lateinit var mAdapter : ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +27,11 @@ class ProductListActivity : BaseActivity() {
                 if (response.isSuccessful){
 
                     val br = response.body()!!
-                    mProductList.addAll((br.data.products))
+                    mProductList.addAll(br.data.products)
+
+                    mAdapter.notifyDataSetChanged()
+
+
                 }
 
             }
@@ -30,8 +39,10 @@ class ProductListActivity : BaseActivity() {
             override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
 
             }
-
-
         })
+
+        mAdapter = ProductAdapter(mContext, mProductList)
+        productRecyclerView.adapter = mAdapter
+        productRecyclerView.layoutManager = LinearLayoutManager(mContext)
     }
 }
