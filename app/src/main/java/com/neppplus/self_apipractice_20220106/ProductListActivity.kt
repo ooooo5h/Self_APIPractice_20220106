efcbf3fc27bc0506877bc5ac7cfa2900d4282974
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 13:06:36 +0900

    실제 상품목록을 뿌려줘야하는데, api 기능이 없네?

--- app/src/main/java/com/neppplus/self_apipractice_20220106/ProductListActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/ProductListActivity.kt
@@ -2,10 +2,16 @@ package com.neppplus.self_apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.self_apipractice_20220106.models.ProductData
 
 class ProductListActivity : BaseActivity() {
+
+    val mProductList = ArrayList<ProductData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_list)
+
+        apiList.
     }
 }
