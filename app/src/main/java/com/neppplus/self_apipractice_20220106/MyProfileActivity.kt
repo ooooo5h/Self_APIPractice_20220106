a1fb539887fb4c21d85670fabfee25fd4848c056
ooooo5h <ooooo5h91@gmail.com>
2022-01-07 12:47:07 +0900

    내 프로필 상세보기 화면에 닉네임과 프로필사진 보여주기 완료

--- app/src/main/java/com/neppplus/self_apipractice_20220106/MyProfileActivity.kt
+++ app/src/main/java/com/neppplus/self_apipractice_20220106/MyProfileActivity.kt
@@ -2,8 +2,10 @@ package com.neppplus.self_apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.neppplus.self_apipractice_20220106.models.BasicResponse
 import com.neppplus.self_apipractice_20220106.utils.ContextUtil
+import kotlinx.android.synthetic.main.activity_my_profile.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,6 +17,17 @@ class MyProfileActivity : BaseActivity() {
 
         apiList.getRequestMyInfo(ContextUtil.getToken(mContext)).enqueue(object : Callback<BasicResponse>{
             override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful){
+
+                    val br = response.body()!!
+                    val myInfo = br.data.user
+
+                    txtMyNickname.text = myInfo.nick_name
+                    Glide.with(mContext).load(myInfo.profile_img).into(imgMyProfile)
+
+
+                }
 
             }
 
